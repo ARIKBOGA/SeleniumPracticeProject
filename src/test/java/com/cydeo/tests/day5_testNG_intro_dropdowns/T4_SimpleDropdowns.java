3a6ae144d33a3f4a250ee6730a55eeb1a95879b6
burakarikboga <barikboga42@gmail.com>
2022-05-12 18:21:17 +0300

    Day_5 Class task have been added
    StaleElementRefEx
    RadioButton
    SimpleDropDowns
    TestNG_Intro
    TestNG_Selenium

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T4_SimpleDropdowns.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T4_SimpleDropdowns.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -53,6 +54,11 @@ public class T4_SimpleDropdowns {
 
         //Assert.assertEquals(stateDropdown.getFirstSelectedOption().getText(), "Select a State");
 
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.close();
     }
 
 }
