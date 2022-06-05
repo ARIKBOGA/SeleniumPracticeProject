c4b9590b5d3b8cdd96f7f27ae63a426774adfca0
burakarikboga <barikboga42@gmail.com>
2022-06-06 01:28:36 +0300

    TestBase.java class has been modified and gained very useful methods for the child classes.

--- src/test/java/com/cydeo/tests/base/TestBase.java
+++ src/test/java/com/cydeo/tests/base/TestBase.java
@@ -1,22 +1,62 @@
 package com.cydeo.tests.base;
 
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public abstract class TestBase {
 
     protected WebDriver driver;
+    protected WebDriverWait wait;
 
     @BeforeMethod
     public void setupDriver() {
         driver = Driver.getDriver();
+        wait = new WebDriverWait(driver, 10);
     }
+
 
     @AfterMethod
     public void tearDown() {
         Driver.closeDriver();
     }
 
+    public void clickFunction(WebElement clickElement) {
+        wait.until(ExpectedConditions.elementToBeClickable(clickElement));
+        clickElement.click();
+    }
+
+    public void sendKeysFunction(WebElement sendKeysElement, String value) {
+        wait.until(ExpectedConditions.visibilityOf(sendKeysElement));
+        sendKeysElement.sendKeys(value);
+    }
+
+    public void selectElementFunction(WebElement dropdown, String visibleText) {
+        Select select = new Select(dropdown);
+        select.selectByVisibleText(visibleText);
+    }
+
+    public void assertionFunction(WebElement alertMessageElement, String expectedMessage) {
+        wait.until(ExpectedConditions.visibilityOf(alertMessageElement));
+        Assert.assertEquals(alertMessageElement.getText(), expectedMessage);
+        System.out.println("My message: " + alertMessageElement.getText());
+    }
+
+    public void executeJavascript(String JSCode, WebElement element) {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript(JSCode, element);
+    }
+
+    public void actionsMoveElement(WebElement element) {
+        Actions actions = new Actions(driver);
+        actions.moveToElement(element).perform();
+    }
 }
