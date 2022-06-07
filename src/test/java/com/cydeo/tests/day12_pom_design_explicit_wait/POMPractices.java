d9b646157477fa40fba64c44270746652ee789e5
burakarikboga <barikboga42@gmail.com>
2022-06-07 14:59:08 +0300

    TestBase class has been modified by adding new method and by changing one of the methods name

--- src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/POMPractices.java
+++ src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/POMPractices.java
@@ -2,7 +2,6 @@ package com.cydeo.tests.day12_pom_design_explicit_wait;
 
 import com.cydeo.pages.LibraryLoginPage;
 import com.cydeo.tests.base.TestBase;
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -24,12 +23,13 @@ public class POMPractices extends TestBase {
         //3- Do not enter any information
         //4- Click to “Sign in” button
 
-        libraryLoginPage.signInButton.click();
+        //libraryLoginPage.signInButton.click();
+        clickFunction(libraryLoginPage.signInButton);
 
         //5- Verify expected error is displayed:
         //Expected: This field is required.
-        Assert.assertTrue(libraryLoginPage.fieldRequiredErrorMessage.isDisplayed());
-
+        //Assert.assertTrue(libraryLoginPage.fieldRequiredErrorMessage.isDisplayed());
+        isDisplayedFunction(libraryLoginPage.fieldRequiredErrorMessage);
         //Driver.closeDriver();
     }
 
@@ -40,13 +40,15 @@ public class POMPractices extends TestBase {
         //2- Go to: https://library1.cydeo.com/
         //3- Enter invalid email format
 
-        libraryLoginPage.inputUsername.sendKeys("somethingwrong");
+        //libraryLoginPage.inputUsername.sendKeys("somethingwrong");
+        sendKeysFunction(libraryLoginPage.inputUsername, "somethingwrong");
 
-        libraryLoginPage.signInButton.click();
-
+        //libraryLoginPage.signInButton.click();
+        clickFunction(libraryLoginPage.signInButton);
         //4- Verify expected error is displayed:
         //Expected: Please enter a valid email address.
-        Assert.assertTrue(libraryLoginPage.enterValidEmailErrorMessage.isDisplayed());
+        //Assert.assertTrue(libraryLoginPage.enterValidEmailErrorMessage.isDisplayed());
+        isDisplayedFunction(libraryLoginPage.enterValidEmailErrorMessage);
 
         //Driver.closeDriver();
 
@@ -59,17 +61,16 @@ public class POMPractices extends TestBase {
         //2- Go to: https://library1.cydeo.com
         //3- Enter incorrect username or incorrect password
 
-        libraryLoginPage.inputUsername.sendKeys("wrong@username.com");
-        libraryLoginPage.inputPassword.sendKeys("wrongpassword");
+        //libraryLoginPage.inputUsername.sendKeys("wrong@username.com");
+        sendKeysFunction(libraryLoginPage.inputUsername, "wrong@username.com");
+        //libraryLoginPage.inputPassword.sendKeys("wrongpassword");
+        sendKeysFunction(libraryLoginPage.inputPassword, "wrongpassword");
 
         libraryLoginPage.signInButton.click();
 
         //4- Verify title expected error is displayed:
         //Expected: Sorry, Wrong Email or Password
-        Assert.assertTrue(libraryLoginPage.wrongEmailOrPasswordErrorMessage.isDisplayed());
-        //Driver.closeDriver();
-
+        //Assert.assertTrue(libraryLoginPage.wrongEmailOrPasswordErrorMessage.isDisplayed());
+        isDisplayedFunction(libraryLoginPage.wrongEmailOrPasswordErrorMessage);
     }
-
-
 }
