c209a33cbdc1a243fef0026e5452eb0437ad1bf2
burakarikboga <barikboga42@gmail.com>
2022-06-09 02:01:19 +0300

    doubleClickPage object has been converted to an instance variable

--- src/test/java/com/cydeo/tests/day13_review_and_practices/T1_DoubleClick.java
+++ src/test/java/com/cydeo/tests/day13_review_and_practices/T1_DoubleClick.java
@@ -8,18 +8,19 @@ import org.testng.annotations.Test;
 
 public class T1_DoubleClick extends TestBase {
 
+    DoubleClickPage doubleClickPage = new DoubleClickPage();
+
     @Test
     public void t1_double_click_test() {
         //1. Goto
         //https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_ev_ondblclick2
         driver.get("https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_ev_ondblclick2");
+
         //2. Switch to iframe.
-        driver.switchTo().frame("iframeResult");
+        driver.switchTo().frame(doubleClickPage.iframe);
 
         //3. Double-click on the text “Double-click me to change my text color.”
-        DoubleClickPage doubleClickPage = new DoubleClickPage();
         Actions actions = new Actions(driver);
-
         actions.doubleClick(doubleClickPage.textToDoubleClick).perform();
 
         //4. Assert:Text’s “style” attribute value contains “red”.
