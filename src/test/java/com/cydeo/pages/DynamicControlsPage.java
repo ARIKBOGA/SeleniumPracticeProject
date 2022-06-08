6cd9886b208ccd2e5311c5da15f481e9b5205da2
burakarikboga <barikboga42@gmail.com>
2022-06-09 01:58:26 +0300

    codes have been formatted

--- src/test/java/com/cydeo/pages/DynamicControlsPage.java
+++ src/test/java/com/cydeo/pages/DynamicControlsPage.java
@@ -10,16 +10,21 @@ public class DynamicControlsPage {
     //@FindBy(xpath = "//button[.='Remove']
     @FindBy(css = "form#checkbox-example > button")
     public WebElement removeButton;
+
     @FindBy(css = "div#loading")
     public WebElement loadingBar;
     //@FindBy(css = "input[label='blah']")
+
     @FindBy(css = "input[type='checkbox']")
     public WebElement checkbox;
     //@FindBy(xpath = "//p[@id='message']")
+
     @FindBy(css = "p[id='message']")
     public WebElement message;
+
     @FindBy(xpath = "//button[.='Enable']")
     public WebElement enableButton;
+
     @FindBy(css = "input[type='text']")
     public WebElement inputBox;
 
